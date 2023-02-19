26fec1a8a8a78c2e45ab9654c931a15488a527cf
dokyunim <imjoshua9316@gmail.com>
2023-02-19 14:35:41 +0900

    - babel.config.js, tsconfig.json 업데이트

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+      [
+          'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@api': './src/api',
+            '@component': './src/component',
+            '@hook': './src/hook',
+            '@navigation': './src/navigation',
+            '@screen': './src/screen',
+            '@service': './src/service',
+            '@type': './src/type'
+          }
+        }
+      ],
+      ['styled-components'],
+  ]
 };
