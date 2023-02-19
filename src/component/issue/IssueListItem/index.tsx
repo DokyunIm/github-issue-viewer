5b6bca7562eace3c4ccc7993d850335945736186
dokyunim <imjoshua9316@gmail.com>
2023-02-19 16:34:27 +0900

    - 상세 화면 수정 - github repo issue 조회 기능 반영

--- src/component/issue/IssueListItem/index.tsx
+++ src/component/issue/IssueListItem/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import styled from 'styled-components/native';
+import {NavigationProp, useNavigation} from '@react-navigation/native';
+import {RootStackParams} from '@type/navigation.type';
 import IssueHeader from '@component/issue/IssueHeader';
-import {Issue} from '@type/issue.type';
 import ADBannerListItem from '@component/ad/ADBannerListItem';
+import {Issue} from '@type/issue.type';
 
 const Wrapper = styled.TouchableOpacity`
   width: 100%;
@@ -13,8 +15,14 @@ interface IssueListItemProps {
   data: Issue;
 }
 function IssueListItem({index, data}: IssueListItemProps) {
+  const navigation = useNavigation<NavigationProp<RootStackParams>>();
+
+  const onPress = () => {
+    navigation.navigate('IssuePost', {issueNumber: data.number});
+  };
+
   return (
-    <Wrapper>
+    <Wrapper onPress={onPress}>
       <IssueHeader data={data} />
       {index === 4 && <ADBannerListItem />}
     </Wrapper>
--- src/screen/IssuePost/index.tsx
+++ src/screen/IssuePost/index.tsx
@@ -1,11 +1,52 @@
 import React from 'react';
+import {ScrollView, View} from 'react-native';
+import {RouteProp, useRoute} from '@react-navigation/native';
 import SafeAreaView from '@component/common/SafeAreaView';
+import useIssuePost from '@hook/useIssuePost';
+import {RootStackParams} from '@type/navigation.type';
+import IssueHeader from '@component/issue/IssueHeader';
 import {CText} from '@component/common/Text';
+import Markdown from 'react-native-markdown-display';
 
 function IssuePost() {
+  const route = useRoute<RouteProp<RootStackParams, 'IssuePost'>>();
+  const {data, isLoading, isError} = useIssuePost({
+    owner: 'angular',
+    repo: 'angular-cli',
+    issueNumber: route.params.issueNumber,
+  });
+
+  if (isLoading) {
+    return (
+      <SafeAreaView>
+        <View style={{alignItems: 'center', paddingTop: 24}}>
+          <CText>이슈를 가져오고 있습니다...</CText>
+        </View>
+      </SafeAreaView>
+    );
+  }
+
+  if (isError) {
+    return (
+      <SafeAreaView>
+        <View style={{alignItems: 'center', paddingTop: 24}}>
+          <CText>이슈 조회 중 오류가 발생하였습니다.</CText>
+        </View>
+      </SafeAreaView>
+    );
+  }
+
   return (
     <SafeAreaView>
-      <CText>이슈 상세 화면</CText>
+      <ScrollView
+        bounces={false}
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+        style={{flex: 1, paddingHorizontal: 12}}>
+        <IssueHeader data={data!} />
+        {/*@ts-ignore*/}
+        <Markdown>{data?.content}</Markdown>
+      </ScrollView>
     </SafeAreaView>
   );
 }
