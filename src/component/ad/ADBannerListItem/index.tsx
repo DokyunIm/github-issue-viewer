3e9de5f4612a50fde0af1727721e8003ac30fc61
dokyunim <imjoshua9316@gmail.com>
2023-02-19 15:53:45 +0900

    - 배너 광고 클릭 이벤트, 인앱 웹뷰 컴포넌트 추가

--- src/component/ad/ADBannerListItem/index.tsx
+++ src/component/ad/ADBannerListItem/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import {Image, TouchableWithoutFeedback} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {RootStackParams} from '@type/navigation.type';
 
 function ADBannerListItem() {
+  const navigation = useNavigation<StackNavigationProp<RootStackParams>>();
+
+  const onPress = () => {
+    navigation.navigate('WebviewInApp', {
+      url: 'https://thingsflow.com/ko/home',
+    });
+  };
+
   return (
-    <TouchableWithoutFeedback>
+    <TouchableWithoutFeedback onPress={onPress}>
       <Image
         source={{
           uri: 'https://hellobot-test.s3.ap-northeast-2.amazonaws.com/image/01fdd797-0477-4717-8d70-8551150463f7',
--- src/screen/WebviewInApp/index.tsx
+++ src/screen/WebviewInApp/index.tsx
@@ -1,11 +1,79 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
+import {BackHandler} from 'react-native';
+import {WebView} from 'react-native-webview';
+import styled from 'styled-components/native';
 import SafeAreaView from '@component/common/SafeAreaView';
 import {CText} from '@component/common/Text';
+import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {RootStackParams} from '@type/navigation.type';
+
+const Header = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CloseButton = styled.TouchableOpacity`
+  left: 8px;
+  position: absolute;
+`;
+
+const UrlView = styled.View`
+  width: 50%;
+`;
 
 function WebviewInApp() {
+  const webView = useRef<WebView>(null);
+  const navigation = useNavigation<StackNavigationProp<RootStackParams>>();
+  const route = useRoute<RouteProp<RootStackParams, 'WebviewInApp'>>();
+
+  const onPressHWBackButton = () => {
+    if (webView.current) {
+      webView.current.goBack();
+      return true;
+    }
+    navigation.goBack();
+    return false;
+  };
+
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', onPressHWBackButton);
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', onPressHWBackButton);
+    };
+  });
+
+  const onPressClose = () => {
+    navigation.goBack();
+  };
+
   return (
-    <SafeAreaView>
-      <CText>인앱 웹뷰 화면</CText>
+    <SafeAreaView edges={['top', 'bottom']}>
+      <Header>
+        <CloseButton onPress={onPressClose}>
+          <CText size={16}>X</CText>
+        </CloseButton>
+        <UrlView>
+          {route.params?.url && (
+            <CText size={14} numberOfLines={1}>
+              {route.params?.url}
+            </CText>
+          )}
+        </UrlView>
+      </Header>
+      {route.params?.url && (
+        <WebView
+          ref={webView}
+          nestedScrollEnabled
+          cacheEnabled={false}
+          showsHorizontalScrollIndicator={false}
+          showsVerticalScrollIndicator={false}
+          allowsFullscreenVideo
+          allowsInlineMediaPlayback
+          source={{uri: encodeURI(route.params.url)}}
+        />
+      )}
     </SafeAreaView>
   );
 }
