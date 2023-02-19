c711eb5e81606b85e8b1948c4bb9b5355950a4e5
dokyunim <imjoshua9316@gmail.com>
2023-02-19 14:43:17 +0900

    - 다크모드 비활성화, Text, TouchableOpacity defaultProps 수정, react-query Provider 추가

--- index.js
+++ index.js
@@ -2,8 +2,14 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Text, TouchableOpacity} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+Text.defaultProps = Text.defaultProps || {};
+Text.defaultProps.maxFontSizeMultiplier = 1.2;
+
+TouchableOpacity.defaultProps = TouchableOpacity.defaultProps || {};
+TouchableOpacity.defaultProps.activeOpacity = 0.8;
 
 AppRegistry.registerComponent(appName, () => App);
